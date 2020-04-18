0d2c3f6b455061e86c32ee8fe3c7f600c7a66259
ryooit <ryooit70@gmail.com>
2020-04-18 19:45:13 +0900

    Edit settings

--- trendboard/settings.py
+++ trendboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3e&4ym$ezwq9^6#855w1_=#vc95-+dd-9%1k40_*+-(t#7ft%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
