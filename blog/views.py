78d5690e47d69d1613f5483d5b7ce3eeb5b3074b
ryooit <ryooit70@gmail.com>
2020-04-20 11:59:07 +0900

    Edit post_list template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('published_dt')
+    return render(request, 'blog/post_list.html', {'posts': posts})
