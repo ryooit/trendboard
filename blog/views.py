d938f71fd9ff34c4e1f8a90985d40d241e93de59
ryooit <ryooit70@gmail.com>
2020-04-20 13:51:59 +0900

    Add more template and css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -6,3 +6,8 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('published_dt')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
